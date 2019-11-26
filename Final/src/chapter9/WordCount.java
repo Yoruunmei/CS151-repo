01c26296a5ab16e4e382ed5d4456e7ddb8914222
victorshih <victorshih@192.168.0.16>
2019-11-25 17:44:16 -0800

    Nonterminaltest moved to a callable.

--- Final/src/chapter9/WordCount.java
+++ Final/src/chapter9/WordCount.java
@@ -1,47 +1,19 @@
 package chapter9;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.awt.List;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.concurrent.*;
 
 public class WordCount {
 
-	
-	// java -classpath . chapter9.WordCount /Users/victorshih/Documents/SJSU/Yr\ 3\ -\ Semester\ 1/CS151/Exam/Shih_Victor_final/Test\ Files/File1.txt
-	public static void main(String[] args) {
-//		int numOfFiles = args.length;
-//		ArrayList<File> listOfFiles = new ArrayList<File>();
-//		for (int i = 0; i < numOfFiles; i++) {
-//			listOfFiles.add(new File(args[i]));
-//		}
-//		ExecutorService service = Executors.newFixedThreadPool(numOfFiles);
-		
-		ExecutorService service = Executors.newFixedThreadPool(1);
-		// Make array of numOfFiles Runnable instances.
-		// Execute each Runnable instance.
-		
-		
-		
-		// counts the words with their length are greater than or equal 4
-		/** read a word. 
-		 * if word length is >= 4 : increase count by 1.
-		 * else : do nothing
-		 * read next word.
-		 */
-		
-		// split by spaces: String input = "Hello I'm your String";
-        // String[] parts = input.split( " " );
-		String nonTerminalTestFile = "/Users/victorshih/Documents/SJSU/Yr 3 - Semester 1/CS151/CS151-repo/Final/src/chapter9/NonTerminalTest";
-		Runnable r1 = () -> {
+	public static Callable<Integer> countWordsWithLength(String filePath) {
+		return () -> {
+			int numOfWordsGT4 = 0;	
 			try {
-				BufferedReader br = new BufferedReader(new FileReader(new File(nonTerminalTestFile)));
+				BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
 //				BufferedReader br = new BufferedReader(new FileReader(listOfFiles.get(0))); // get first file
 				String line;
-				int numOfWordsGT4 = 0;	
 				
 				while ((line = br.readLine()) != null) {	// Continuously read next line.
 					String [] wordsInLine = line.split(" "); // Separate line into individual words.
@@ -55,7 +27,7 @@ public class WordCount {
 					}
 				}
 				//System.out.println(listOfFiles.get(0).getName() + " : " + numOfWordsGT4);
-				System.out.println("NonTerminalTest : " + numOfWordsGT4);
+				
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -63,8 +35,45 @@ public class WordCount {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			return numOfWordsGT4;
 		};
-		service.execute(r1);
+	}
+	
+	
+	// java -classpath . chapter9.WordCount /Users/victorshih/Documents/SJSU/Yr\ 3\ -\ Semester\ 1/CS151/Exam/Shih_Victor_final/Test\ Files/File1.txt
+	public static void main(String[] args) {
+//		int numOfFiles = args.length;
+//		ArrayList<File> listOfFiles = new ArrayList<File>();
+//		for (int i = 0; i < numOfFiles; i++) {
+//			listOfFiles.add(new File(args[i]));
+//		}
+//		ExecutorService service = Executors.newFixedThreadPool(numOfFiles);		
+		ExecutorService service = Executors.newFixedThreadPool(1);
+		
+		// Make array of numOfFiles Runnable instances.
+		// Execute each Runnable instance.	
+		
+		// counts the words with their length are greater than or equal 4
+		/** read a word. 
+		 * if word length is >= 4 : increase count by 1.
+		 * else : do nothing
+		 * read next word.
+		 */	
+		// split by spaces: String input = "Hello I'm your String";
+		String nonTerminalTestFile = "/Users/victorshih/Documents/SJSU/Yr 3 - Semester 1/CS151/CS151-repo/Final/src/chapter9/NonTerminalTest";
+		ArrayList<Callable<Integer>> tasks = new ArrayList<Callable<Integer>>();
+		tasks.add(countWordsWithLength(nonTerminalTestFile));
+		
+		try {
+			ArrayList<Future<Integer>> results = (ArrayList<Future<Integer>>) service.invokeAll(tasks);
+			for (Future<Integer> result : results) {
+				System.out.println("NonTerminalTest : " + result.get().toString());
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace(); // error in result.get()
+		}	
 		
 		
 		service.shutdown();
