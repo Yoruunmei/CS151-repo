d58da24a56e96a5b9a3ca23427e30d0671fd5751
victorshih <victorshih@192.168.0.16>
2019-11-25 22:42:46 -0800

    (from terminal). Finished with callable counting method for words >=4

--- Final/src/chapter9/WordCount.java
+++ Final/src/chapter9/WordCount.java
@@ -1,18 +1,22 @@
 package chapter9;
 
-import java.awt.List;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.concurrent.*;
 
 public class WordCount {
 
-	public static Callable<Integer> countWordsWithLength(String filePath) {
+	/**
+	 * Counts number of words with length >= 4 within a file. 
+	 * @param file the file to be analyzed. 
+	 * @return the instance of a Callable<Integer> that returns the value of the number of words with length >= 4.
+	 */
+	public static Callable<Integer> countWordsWithLength(File file) {
 		return () -> {
 			int numOfWordsGT4 = 0;	
 			try {
-				BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
-//				BufferedReader br = new BufferedReader(new FileReader(listOfFiles.get(0))); // get first file
+//				BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
+				BufferedReader br = new BufferedReader(new FileReader(file)); // get first file
 				String line;
 				
 				while ((line = br.readLine()) != null) {	// Continuously read next line.
@@ -41,33 +45,49 @@ public class WordCount {
 	
 	
 	// java -classpath . chapter9.WordCount /Users/victorshih/Documents/SJSU/Yr\ 3\ -\ Semester\ 1/CS151/Exam/Shih_Victor_final/Test\ Files/File1.txt
+	/** Everytime a change is saved, need to :
+	 * <go to dir with .java file>
+	 * <compile java file>
+	 * 	javac WordCount.java
+	 * 	cd ..
+	 * 	java -classpath . chapter9.WordCount <args here with spaces betw>
+	 * 
+	 */
+	
+	
 	public static void main(String[] args) {
-//		int numOfFiles = args.length;
+		
+		// TERMINAL INPUT
+		int numOfFiles = args.length;
+		ArrayList<File> listOfFiles = new ArrayList<File>();
+		for (int i = 0; i < numOfFiles; i++) {
+			listOfFiles.add(new File(args[i]));
+		}
+		ExecutorService service = Executors.newFixedThreadPool(numOfFiles);		
+		
+		// NON TERMINAL INPUT (for debugging).
+//		ExecutorService service = Executors.newFixedThreadPool(2);
+//		
 //		ArrayList<File> listOfFiles = new ArrayList<File>();
-//		for (int i = 0; i < numOfFiles; i++) {
-//			listOfFiles.add(new File(args[i]));
-//		}
-//		ExecutorService service = Executors.newFixedThreadPool(numOfFiles);		
-		ExecutorService service = Executors.newFixedThreadPool(1);
+//		int numOfFiles = 2;
+//		listOfFiles.add(new File("/Users/victorshih/Documents/SJSU/Yr 3 - Semester 1/CS151/CS151-repo/Final/src/chapter9/File1.txt"));
+//		listOfFiles.add(new File("/Users/victorshih/Documents/SJSU/Yr 3 - Semester 1/CS151/CS151-repo/Final/src/chapter9/File2.txt"));
+		// end block here for non terminal input.
 		
-		// Make array of numOfFiles Runnable instances.
-		// Execute each Runnable instance.	
-		
-		// counts the words with their length are greater than or equal 4
-		/** read a word. 
-		 * if word length is >= 4 : increase count by 1.
-		 * else : do nothing
-		 * read next word.
-		 */	
-		// split by spaces: String input = "Hello I'm your String";
-		String nonTerminalTestFile = "/Users/victorshih/Documents/SJSU/Yr 3 - Semester 1/CS151/CS151-repo/Final/src/chapter9/NonTerminalTest";
 		ArrayList<Callable<Integer>> tasks = new ArrayList<Callable<Integer>>();
-		tasks.add(countWordsWithLength(nonTerminalTestFile));
+
+		for (int i = 0; i < listOfFiles.size(); i++) {
+			tasks.add(countWordsWithLength(listOfFiles.get(i)));
+		}
+		if (tasks.size() != numOfFiles)
+			System.out.println("# of callables doesn't match # of files");
 		
 		try {
 			ArrayList<Future<Integer>> results = (ArrayList<Future<Integer>>) service.invokeAll(tasks);
+			int i = 0;
 			for (Future<Integer> result : results) {
-				System.out.println("NonTerminalTest : " + result.get().toString());
+				System.out.println(listOfFiles.get(i).getName() + " : " + result.get().toString());
+				i++;
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
