409fc6a8fb0cec8894df373f3ba891fabe33146e
victorshih <victorshih@192.168.0.16>
2019-11-25 23:17:30 -0800

    now ignores excess whitespace.

--- Final/src/chapter9/WordCount.java
+++ Final/src/chapter9/WordCount.java
@@ -6,16 +6,49 @@ import java.util.concurrent.*;
 
 public class WordCount {
 
+	
+	public static Callable<Integer> countWordsWithLengthAtMost4(File file) {
+		return () -> {
+			int numOfWordsGT4 = 0;	
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(file)); // get first file
+				String line;
+				
+				while ((line = br.readLine()) != null) {	// Continuously read next line.
+					
+					String [] wordsInLine = line.trim().replaceAll(" +", " ").split(" "); // Separate line into individual words.
+					
+					for (int i = 0; i < wordsInLine.length; i++) {
+						if (wordsInLine[i].length() <= 4 && wordsInLine[i].trim().length() != 0) {	// Check length of each word. 
+							numOfWordsGT4++;
+						} else {
+							// do nothing.
+						}
+					}
+				}
+				//System.out.println(listOfFiles.get(0).getName() + " : " + numOfWordsGT4);
+				
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return numOfWordsGT4;
+		};
+	}
+	
+	
 	/**
 	 * Counts number of words with length >= 4 within a file. 
 	 * @param file the file to be analyzed. 
 	 * @return the instance of a Callable<Integer> that returns the value of the number of words with length >= 4.
 	 */
-	public static Callable<Integer> countWordsWithLength(File file) {
+	public static Callable<Integer> countWordsWithLengthAtLeast4(File file) {
 		return () -> {
 			int numOfWordsGT4 = 0;	
 			try {
-//				BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
 				BufferedReader br = new BufferedReader(new FileReader(file)); // get first file
 				String line;
 				
@@ -73,11 +106,12 @@ public class WordCount {
 //		listOfFiles.add(new File("/Users/victorshih/Documents/SJSU/Yr 3 - Semester 1/CS151/CS151-repo/Final/src/chapter9/File1.txt"));
 //		listOfFiles.add(new File("/Users/victorshih/Documents/SJSU/Yr 3 - Semester 1/CS151/CS151-repo/Final/src/chapter9/File2.txt"));
 		// end block here for non terminal input.
-		
+		System.out.println("# of words with at least length 4 in each file:");
+		// For >=4 method
 		ArrayList<Callable<Integer>> tasks = new ArrayList<Callable<Integer>>();
 
 		for (int i = 0; i < listOfFiles.size(); i++) {
-			tasks.add(countWordsWithLength(listOfFiles.get(i)));
+			tasks.add(countWordsWithLengthAtLeast4(listOfFiles.get(i)));
 		}
 		if (tasks.size() != numOfFiles)
 			System.out.println("# of callables doesn't match # of files");
@@ -95,7 +129,27 @@ public class WordCount {
 			e.printStackTrace(); // error in result.get()
 		}	
 		
+		// For <=4 method
+		ArrayList<Callable<Integer>> tasks2 = new ArrayList<Callable<Integer>>();
+		System.out.println("# of words with at most length 4 in each file:");
+		for (int i = 0; i < listOfFiles.size(); i++) {
+			tasks2.add(countWordsWithLengthAtMost4(listOfFiles.get(i)));
+		}
+		if (tasks2.size() != numOfFiles)
+			System.out.println("# of callables doesn't match # of files");
 		
+		try {
+			ArrayList<Future<Integer>> results = (ArrayList<Future<Integer>>) service.invokeAll(tasks2);
+			int i = 0;
+			for (Future<Integer> result : results) {
+				System.out.println(listOfFiles.get(i).getName() + " : " + result.get().toString());
+				i++;
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace(); // error in result.get()
+		}	
 		service.shutdown();
 	}
 }
